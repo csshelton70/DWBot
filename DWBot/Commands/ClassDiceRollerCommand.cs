7ba8432e9ca9c0550e49cb3d251b2f494e528cfc
csshelton70 <chris.scott.shelton@gmail.com>
2022-07-16 16:55:58 -0400

    Initial setup (#1)
    * added solution
    
    * Added basic roll command

---
+++ DWBot/Commands/ClassDiceRollerCommand.cs
@@ -0,0 +1,63 @@
+﻿using Dice;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWBot.Commands
+{
+    public class ClassDiceRollerCommand : BaseCommandModule
+    {
+        public Random Rng { private get; set; }
+
+
+        [Command("roll")]
+        public async Task GreetCommand(CommandContext ctx, string cmd)
+        {
+            RollResult result = Roller.Roll(cmd);
+            await ctx.RespondAsync(string.Format("Dice Rolled [{0}] = {1}", BuildResult(result), result.Value));
+        }
+
+        private string BuildResult(RollResult diceResult)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (DieResult die in diceResult.Values)
+            {
+                if (die.DieType == DieType.Special)
+                {
+                    switch ((SpecialDie)die.Value)
+                    {
+                        case SpecialDie.Add:
+                            sb.Append(" + ");
+                            break;
+                        case SpecialDie.Subtract:
+                            sb.Append(" - ");
+                            break;
+                        case SpecialDie.Multiply:
+                            sb.Append(" * ");
+                            break;
+                        case SpecialDie.Divide:
+                            sb.Append(" / ");
+                            break;
+                        case SpecialDie.OpenParen:
+                            sb.Append("(");
+                            break;
+                        case SpecialDie.CloseParen:
+                            sb.Append(")");
+                            break;
+                    }
+                }
+                else
+                {
+                    sb.Append(die.Value);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ DWBot/Program.cs
@@ -0,0 +1,52 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DWBot.Commands;
+using Emzi0767.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static string discordBotToken;
+
+
+        static void Main(string[] args)
+        {
+            discordBotToken = ConfigurationManager.AppSettings["discord_token"];
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var config = new DiscordConfiguration()
+            {
+                Token = discordBotToken,
+                TokenType = TokenType.Bot,
+                MinimumLogLevel = LogLevel.Debug,
+                // Critical, Error, Warning, Information,Debug, Trace
+                //Intents = DiscordIntents.All
+            };
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!" }
+            };
+
+            var discord = new DiscordClient(config);
+
+            var commands = discord.UseCommandsNext(commandsConfig);
+
+            commands.RegisterCommands<ClassDiceRollerCommand>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+    }
+}
