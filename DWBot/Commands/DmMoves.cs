15d35cb2bd6295e57be8549ce75dcb7d7a9f1674
Scott Shelton <chris.scott.shelton@gmail.com>
2022-07-16 21:11:27 -0400

    Added scaffolding

---
+++ DWBot/Commands/DmMoves.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace DWBot.Commands
+{
+    public class DmMoves
+    {
+        [Command("gmmoves")]
+        [Description("Returns a list of GM Moves")]
+        [Aliases("dmmmoves")]
+        public async Task gmmoves(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":fire:");
+            await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
+        }
+    }
+}
